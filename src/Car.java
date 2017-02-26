ebc65df67a3399e903fff888d2a105b89354fc26
Matt <tenaciouscree@hotmail.com>
2017-02-26 13:39:02 +0000

    Organising required methods for Car, SmallCar, LargeCar classes

--- src/Car.java
+++ src/Car.java
@@ -2,4 +2,17 @@
  * Created by Cree on 26/02/2017.
  */
 public abstract class Car {
+
+    private RegistrationNumber registrationNumber;
+    private int fuelRemaining;
+    private boolean rentalStatus;
+
+    public RegistrationNumber getRegistration();
+    public int getCapacity();
+    public int getFuelRemaining();
+    public boolean isFull();
+    public boolean isRented();
+    public boolean addFuel(int amount);
+
+
 }
--- src/SmallCar.java
+++ src/SmallCar.java
@@ -2,4 +2,18 @@
  * Created by Cree on 24/02/2017.
  */
 public class SmallCar extends Car {
+
+    private static final int TANK_CAPACITY = 49;
+    private RegistrationNumber registrationNumber;
+    private int fuelRemaining;
+    private boolean rentalStatus;
+
+    public RegistrationNumber getRegistration();
+    public int getCapacity();
+    public int getFuelRemaining();
+    public boolean isFull();
+    public boolean isRented();
+    public boolean addFuel(int amount);
+    public int drive(int distance);
+
 }
